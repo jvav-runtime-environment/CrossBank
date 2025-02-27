8f25cbeb6cd7e6bb1fe9d51406c95275b87b1d69
jvav-runtime-environment <dannybxx@qq.com>
2025-02-27 11:24:07 +0800

    优化

--- src/main/java/cn/JvavRE/crossBank/utils/Message.java
+++ src/main/java/cn/JvavRE/crossBank/utils/Message.java
@@ -7,8 +7,13 @@ import net.kyori.adventure.text.minimessage.MiniMessage;
 import org.bukkit.command.CommandSender;
 
 public class Message {
+    private static final Component header = MiniMessage.miniMessage().deserialize("<gradient:#0096FF:#00ff96>[CrossBank] </gradient>");
+
     public static void send(CommandSender sender, MessageKey key, Object... args){
         Component component = MiniMessage.miniMessage().deserialize(LanguageConfig.get(key).formatted(args));
-        sender.sendMessage(component);
+        sender.sendMessage(Component.text()
+                .append(header)
+                .append(component)
+        );
     }
 }
